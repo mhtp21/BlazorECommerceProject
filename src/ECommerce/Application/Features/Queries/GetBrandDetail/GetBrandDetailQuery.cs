ef4b459a8d3e6128a534388da1dea1ec59d65c1e
Muhammed <mhatipglu@gmail.com>
2022-06-25 15:42:55 +0300

    Additional Actions

---
+++ src/ECommerce/Application/Features/Queries/GetBrandDetail/GetBrandDetailQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Common.Models.Queries;
+
+namespace Application.Features.Queries.GetBrandDetail
+{
+    public class GetBrandDetailQuery : IRequest<BrandDetailViewModel>
+    {
+        public GetBrandDetailQuery(Guid brandId, string brandName)
+        {
+            BrandId = brandId;
+            BrandName = brandName;
+        }
+
+        public Guid BrandId { get; set; }
+        public string BrandName { get; set; }
+
+
+    }
+}
